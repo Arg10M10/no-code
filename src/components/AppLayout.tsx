9c06d5e3ff9d0884928fd2777397e28a76189773
[dyad] <rodrigopepe2006@gmail.com>
2026-03-17 15:50:02 -0400

    [dyad] wrote 2 file(s)

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import AppSidebar from "./AppSidebar";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -14,6 +14,13 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   const isPreview = location.pathname.startsWith("/preview");
   const isElectron = typeof window.electronAPI !== 'undefined';
 
+  useEffect(() => {
+    console.log("Is Electron environment:", isElectron);
+    if (!isElectron) {
+      console.warn("Not running in Electron. CustomTitleBar will not be rendered.");
+    }
+  }, [isElectron]);
+
   if (isPreview) {
     return <>{children}</>;
   }
@@ -21,7 +28,7 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
     <div className="flex h-screen w-full overflow-hidden bg-background">
       <AppSidebar />
-      <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative bg-background"> {/* Added bg-background here */}
+      <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative"> {/* Removed bg-background here */}
         {isElectron && <CustomTitleBar />}
         <div className="flex-1 min-h-0 flex flex-col rounded-xl overflow-hidden">
           {isEditor ? (
